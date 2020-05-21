ba39e3eeae2a3b715c75a223a55906ed9fb9221f
yichuancq <1012027293@qq.com>
2020-05-21 14:26:04 +0800

    添加 子模块

---
+++ auth-server/src/main/java/com/example/AuthApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yichuan
+ */
+@SpringBootApplication
+public class AuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+}
--- jwt-admin/src/main/java/com/example/controller/UserController.java
+++ jwt-admin/src/main/java/com/example/controller/UserController.java
@@ -19,7 +19,6 @@ import java.util.Map;
 @RestController
 public class UserController {
 
-
     /**
      * sayHello
      *
