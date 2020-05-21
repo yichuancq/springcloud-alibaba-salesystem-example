37dfdfe159f81e1e5cedec8655b42de51305bcc8
yichuancq <1012027293@qq.com>
2020-05-21 22:08:15 +0800

    fix README.md

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # springcloud-alibaba-salesystem-example
 
-### 一个基于spring-cloud分布式权限控制，资源访问校验，微服务治理解决方案综合案例
+### 一个基于spring-cloud分布式权限控制，资源访问校验，微服务治理解决方案的综合案例
---
+++ commons/src/main/java/com/example/dto/UserDto.java
@@ -0,0 +1,20 @@
+package com.example.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+/**
+ * UserDto
+ *
+ * @author yichuan
+ */
+@Getter
+@Setter
+@ToString
+public class UserDto implements Serializable {
+    private String userName;
+    private String password;
+}
