64d8778b48092585d91d114502ef8c4f6d59bdc9
yichuancq <1012027293@qq.com>
2020-05-30 13:20:21 +0800

    服务注册中心

---
+++ eureka-server/src/main/java/com/eureka/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @calss name EurekaServerApplication
+ * @description: EurekaServerApplication
+ * @author: yichuan
+ * @create time: 2020/05/29 23:46
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
